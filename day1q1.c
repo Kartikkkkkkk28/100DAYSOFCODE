243abb16eed3b0cf9b013aeb90598057b3488c00
Kartikkkkkkk28 <kartiksrivastav765@gmail.com>
2025-09-01 19:45:34 +0530

    Add files via upload

---
+++ day1q1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int a;
+    int b;
+    printf("enter first number : \n");
+    scanf("%d" , &a);
+    printf("enter second number : \n");
+    scanf("%d",&b);
+    int sum = a+b;
+    printf( "sum is %d \n ",sum );
+    return 0;
+
+
+    
+
+
+}
