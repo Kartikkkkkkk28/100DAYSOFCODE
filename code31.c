955887b0aa6e935bc9039484d7176dd3b52f4397
Kartikkkkkkk28 <kartiksrivastav765@gmail.com>
2025-09-05 21:25:05 +0530

    Add files via upload

---
+++ code31.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void toBinary(int n) {
+    if (n > 1) {
+        toBinary(n / 2);   
+    }
+    printf("%d", n % 2);   
+}
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    if (n == 0) {
+        printf("Binary: 0\n");
+    } else {
+        printf("Binary: ");
+        toBinary(n);
+        printf("\n");
+    }
+    return 0;
+}
