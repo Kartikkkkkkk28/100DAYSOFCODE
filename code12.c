7cab4460d233ed400cf06628b9d9f822339e1bc3
Kartikkkkkkk28 <kartiksrivastav765@gmail.com>
2025-09-07 12:32:44 +0530

    hi

--- code12.c
+++ code12.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<stdio.h>
 int main (){
     int x;
@@ -18,24 +17,3 @@ int main (){
         }
 
   
-=======
-#include<stdio.h>
-int main (){
-    int x;
-    printf("enter a number ");
-    scanf("%d",&x);
-    if (x>=0){
-        if(x==0){
-            printf("number is zero \n");
-        }
-    else{
-        printf("number is positive \n");
-    }
-    } else{
-        printf("number is negative\n");
-    }
-        
-        }
-
-  
->>>>>>> 955887b0aa6e935bc9039484d7176dd3b52f4397
--- code16.c
+++ code16.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 int main() {
     int a, b, c;
@@ -12,19 +11,4 @@ int main() {
         printf("%d is the largest number.\n", c);
     }
     return 0;
-=======
-#include <stdio.h>
-int main() {
-    int a, b, c;
-    printf("Enter three numbers: ");
-    scanf("%d %d %d", &a, &b, &c);
-    if (a >= b && a >= c) {
-        printf("%d is the largest number.\n", a);
-    } else if (b >= a && b >= c) {
-        printf("%d is the largest number.\n", b);
-    } else {
-        printf("%d is the largest number.\n", c);
-    }
-    return 0;
->>>>>>> 955887b0aa6e935bc9039484d7176dd3b52f4397
 }
