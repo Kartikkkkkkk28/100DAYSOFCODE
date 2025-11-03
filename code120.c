91b7b2ac6e9e28bd38a31e6cbf3edd187cca870b
Kartik Srivastava <kartiksrivastav765@gmail.com>
2025-11-03 18:23:56 +0530

    Update string capitalization logic
    Refactor string processing to capitalize the first letter of each word and convert the rest to lowercase.

--- code120.c
+++ code120.c
@@ -6,18 +6,19 @@ int main() {
     fgets(str, sizeof(str), stdin);
 
     int i = 0;
-    
-    while (str[i] != '\0') {
-        if (isalpha(str[i])) {
-            str[i] = toupper(str[i]);
-            i++;
-            break;
-        }
-        i++;
-    }
+    int newWord = 1;
 
     while (str[i] != '\0') {
-        str[i] = tolower(str[i]);
+        if (isalpha(str[i])) {
+            if (newWord) {
+                str[i] = toupper(str[i]);
+                newWord = 0;
+            } else {
+                str[i] = tolower(str[i]);
+            }
+        } else {
+            newWord = (str[i] == ' ') ? 1 : newWord;
+        }
         i++;
     }
 
